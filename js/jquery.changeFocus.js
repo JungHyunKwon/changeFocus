112d6df27dd612893348120bd84d0b16d09d9494
JungHyunKwon <kjh_3859@naver.com>
2019-07-21 02:08:29 +0900

    published

--- js/jquery.changeFocus.js
+++ js/jquery.changeFocus.js
@@ -2,44 +2,36 @@
  * @author JungHyunKwon
  * @version 1.0.0
  */
-try {
+(function($) {
 	'use strict';
 
-	(function($) {
-		//제이쿼리가 함수일 때
-		if(typeof $ === 'function') {
-			/**
-			 * @name changeFocus
-			 * @since 2019-01-07
-			 * @param {function} callback
-			 * @return {jQuery}
-			 */
-			$.fn.changeFocus = function(callback) {
-				var $firstOfThis = this.first(),
-					hasTabindex = $firstOfThis.attr('tabindex');
-				
-				//tabindex 속성을 가지고 있지 않을 때
-				if(!hasTabindex) {
-					$firstOfThis.attr('tabindex', -1);
-				}
-				
-				$firstOfThis.focus();
-
-				//beforeChange 매개변수가 함수일 때
-				if(typeof callback === 'function') {
-					callback();
-				}
-
-				if(!hasTabindex) {
-					$firstOfThis.removeAttr('tabindex');
-				}
-
-				return this;
-			};
-		}else{
-			throw '제이쿼리가 없습니다.';
+	/**
+	 * @name changeFocus
+	 * @since 2019-01-07
+	 * @param {function} callback
+	 * @return {object}
+	 */
+	$.fn.changeFocus = function(callback) {
+		var $firstThis = this.first(),
+			hasTabindex = $firstThis.attr('tabindex');
+		
+		//tabindex 속성을 가지고 있지 않을 때
+		if(!hasTabindex) {
+			$firstThis.attr('tabindex', -1);
 		}
-	})(window.jQuery);
-}catch(error) {
-	console.error(error);
-}
+		
+		$firstThis.focus();
+
+		//함수일 때
+		if(typeof callback === 'function') {
+			callback();
+		}
+		
+		//tabindex 속성을 가지고 있을 때
+		if(!hasTabindex) {
+			$firstThis.removeAttr('tabindex');
+		}
+
+		return this;
+	};
+})(jQuery);
